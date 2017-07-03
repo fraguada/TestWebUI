090805a2bc60db7e5b399b636beb82979e48d581
Luis Fraguada <luis@mcneel.com>
2017-07-03 13:19:30 +0200

    First commit

---
+++ TestEtoWebkit/app/app.js
@@ -0,0 +1,23 @@
+﻿function SayHi(name)
+{
+    alert("Hi there " + name);
+
+    return "some hi";
+};
+
+function ReturnData(num)
+{
+
+    alert("Data!");
+
+    var arrayNum = [];
+    for (var i = 0; i < num; i++)
+        arrayNum.push(i);
+
+    return "something data";
+};
+
+function GotoAnother(otherUrl)
+{
+    window.location.replace(otherUrl);
+}
